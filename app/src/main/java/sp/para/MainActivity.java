dca5dbd7ca9cac909ea4343e7918da72a082bd52
Jd Banares <jandenzelbanares@gmail.com>
2015-09-27 18:17:05 +0800

    Fixed dependency; Added marker and route line on map

--- app/src/main/java/sp/para/MainActivity.java
+++ app/src/main/java/sp/para/MainActivity.java
@@ -4,9 +4,17 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import org.osmdroid.api.IMapController;
+import org.osmdroid.bonuspack.overlays.Polyline;
+import org.osmdroid.bonuspack.routing.OSRMRoadManager;
+import org.osmdroid.bonuspack.routing.Road;
+import org.osmdroid.bonuspack.routing.RoadManager;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
+
+import org.osmdroid.bonuspack.overlays.Marker;
+
+import java.util.ArrayList;
 
 public class MainActivity extends Activity {
     @Override
@@ -24,7 +32,32 @@ public class MainActivity extends Activity {
 
         IMapController mapController = map.getController();
         mapController.setZoom(18);
-        GeoPoint startPoint = new GeoPoint(14.69161,120.96928);
+        GeoPoint startPoint = new GeoPoint(14.69161, 120.96928);
         mapController.setCenter(startPoint);
+
+        Marker startMarker = new Marker(map);
+        startMarker.setPosition(startPoint);
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+        //startMarker.setIcon(getResources().getDrawable(R.drawable.ic_launcher));
+        startMarker.setTitle("Start point");
+
+        map.getOverlays().add(startMarker);
+        map.invalidate();
+
+        RoadManager roadManager = new OSRMRoadManager();
+
+        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+        waypoints.add(startPoint);
+        GeoPoint endPoint = new GeoPoint(14.695615, 120.971597);
+        waypoints.add(endPoint);
+
+        Road road = roadManager.getRoad(waypoints);
+
+        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, this);
+
+        map.getOverlays().add(roadOverlay);
+
+        map.invalidate();
+
     }
 }
