92364c453b2b9fa43b9e842845d7aaf4f2cd9459
Jd Banares <jandenzelbanares@gmail.com>
2016-02-14 23:25:47 +0800

    Show one commute route extracted from the database

--- app/src/main/java/sp/para/activities/MainActivity.java
+++ app/src/main/java/sp/para/activities/MainActivity.java
@@ -20,6 +20,7 @@ import org.osmdroid.bonuspack.overlays.Marker;
 import java.io.File;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 import sp.para.R;
 import sp.para.models.*;
@@ -46,33 +47,54 @@ public class MainActivity extends Activity {
 //        Marker startMarker = new Marker(map);
 //        startMarker.setPosition(startPoint);
 //        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
-        //startMarker.setIcon(getResources().getDrawable(R.drawable.ic_launcher));
+//        startMarker.setIcon(getResources().getDrawable(R.drawable.ic_launcher));
 //        startMarker.setTitle("Start point");
 
 //        map.getOverlays().add(startMarker);
 //        map.invalidate();
 
-//        RoadManager roadManager = new OSRMRoadManager();
-//
-//        ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
-//        waypoints.add(startPoint);
-//        GeoPoint endPoint = new GeoPoint(14.695615, 120.971597);
-//        waypoints.add(endPoint);
-//
-//        Road road = roadManager.getRoad(waypoints);
-//
-//        Polyline roadOverlay = RoadManager.buildRoadOverlay(road, this);
-
-//        map.getOverlays().add(roadOverlay);
-
-//        map.invalidate();
-
         // Population of Stops
-        Stops.populate(getResources().openRawResource(R.raw.stops));
+//        Stops.populate(getResources().openRawResource(R.raw.stops));
 
+        // Population of Routes
 //        Route.populate(getResources().openRawResource(R.raw.routes));
 
-//        Log.d("-------------APP", "Route size = "+Route.getAll().size());
+        // Population of Trips
+//        Trip.populate(getResources().openRawResource(R.raw.trips));
+
+        // Population of StopTimes
+//        StopTime.populate(getResources().openRawResource(R.raw.stop_times));
+
+        Log.d("-------------APP", "Stops size = " + Stops.getAll().size());
+        Log.d("-------------APP", "Route size = " + Route.getAll().size());
+        Log.d("-------------APP", "Trip size = " + Trip.getAll().size());
+        Log.d("-------------APP", "StopTime size = "+StopTime.getAll().size());
+
+        // Draw Lines
+
+        RoadManager roadManager = new OSRMRoadManager();
+
+//        List<StopTime> stopTimeList = StopTime.getAllByTrip(Trip.getByTripId("724628"));
+        List<StopTime> stopTimeList = StopTime.getAllByTrip(Trip.getByTripId("724674"));
+
+        for(int i = 0; i < stopTimeList.size()-1; i++) {
+            StopTime currLine = stopTimeList.get(i);
+            StopTime nextLine = stopTimeList.get(i+1);
+            ArrayList<GeoPoint> waypoints = new ArrayList<GeoPoint>();
+            GeoPoint startLine = new GeoPoint(currLine.getStop().getLat(),currLine.getStop().getLon());
+            waypoints.add(startLine);
+            GeoPoint endLine = new GeoPoint(nextLine.getStop().getLat(),nextLine.getStop().getLon());
+            waypoints.add(endLine);
+
+            Road road = roadManager.getRoad(waypoints);
+
+            Polyline roadOverlay = RoadManager.buildRoadOverlay(road, this);
+
+            map.getOverlays().add(roadOverlay);
+        }
+
+
+//        map.invalidate();
 
         // Place marker on each stop
         /*
