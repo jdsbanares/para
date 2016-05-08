ee08e10ca9ef0139fd0aac8c9d82829b398685e3
Jd Banares <jandenzelbanares@gmail.com>
2016-05-08 21:15:58 +0800

    Added search in routes fragment

--- app/src/main/java/sp/para/fragments/RoutesFragment.java
+++ app/src/main/java/sp/para/fragments/RoutesFragment.java
@@ -1,13 +1,16 @@
 package sp.para.fragments;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TableLayout;
@@ -26,6 +29,7 @@ public class RoutesFragment extends Fragment {
 
     Button backBtn;
     ListView listRoutes;
+    AutoCompleteTextView routeSearch;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,15 +44,39 @@ public class RoutesFragment extends Fragment {
             }
         });
 
-        listRoutes = (ListView) view.findViewById(R.id.listRoutes);
-
         final ArrayAdapter<Route> routeAdapter = new ArrayAdapter<Route>(getActivity(), android.R.layout.simple_list_item_1, Route.getAll());
+
+        routeSearch = (AutoCompleteTextView) view.findViewById(R.id.routeSearch);
+
+        routeSearch.setAdapter(routeAdapter);
+
+        routeSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                View currView = getActivity().getCurrentFocus();
+                if(currView != null) {
+                    InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+
+                MapFragment mf = (MapFragment) getFragmentManager().findFragmentByTag("map_frag");
+                mf.showExisting(routeAdapter.getItem(position));
+            }
+        });
+
+        listRoutes = (ListView) view.findViewById(R.id.listRoutes);
 
         listRoutes.setAdapter(routeAdapter);
 
         listRoutes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                View currView = getActivity().getCurrentFocus();
+                if(currView != null) {
+                    InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                }
+
                 MapFragment mf = (MapFragment) getFragmentManager().findFragmentByTag("map_frag");
                 mf.showExisting(routeAdapter.getItem(position));
             }
