a9bcff7c533a89a57bab6eb39de3673fc875afda
Jd Banares <jandenzelbanares@gmail.com>
2016-05-18 23:42:03 +0800

    Documentation for PartialStepsFragment

--- app/src/main/java/sp/para/fragments/PartialStepsFragment.java
+++ app/src/main/java/sp/para/fragments/PartialStepsFragment.java
@@ -3,6 +3,7 @@ package sp.para.fragments;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,16 +29,21 @@ public class PartialStepsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.partial_steps_fragment, container, false);
 
+        Log.i("PartialStepsFragment - ", "Setting up Partial Steps Fragment...");
+
         partialFrame = (FrameLayout) view.findViewById(R.id.partialFrame);
         fromTxtFld = (TextView) view.findViewById(R.id.txtFrom);
         toTxtFld = (TextView) view.findViewById(R.id.txtTo);
 
+        // Set values for from and to text views
         fromTxtFld.setText(this.from.getName());
         toTxtFld.setText(this.to.getName());
 
         partialFrame.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.i("PartialStepsFragment - ", "Partial steps fragment clicked!");
+                // Redirect to steps fragment
                 StepsFragment stepsFragment = new StepsFragment();
                 stepsFragment.setInstList(getInstList());
                 FragmentTransaction ft = getFragmentManager().beginTransaction();
@@ -50,21 +56,26 @@ public class PartialStepsFragment extends Fragment {
         return view;
     }
 
+    // Created new instance for PartialStepsFragment
     public static PartialStepsFragment newInstance(Stops from, Stops to, ArrayList<InstructionNode> instList) {
+        Log.i("PartialStepsFragment - ", "Creating new instance of partial steps fragment...");
         PartialStepsFragment newFragment = new PartialStepsFragment();
         newFragment.setInstList(instList);
         newFragment.setPoints(from, to);
         return newFragment;
     }
 
+    // Get instructions list
     public ArrayList<InstructionNode> getInstList() {
         return this.instList;
     }
 
+    // Set instructions list
     public void setInstList(ArrayList<InstructionNode> instList) {
         this.instList = instList;
     }
 
+    // Set origin and destination
     public void setPoints(Stops from, Stops to) {
         this.from = from;
         this.to = to;
