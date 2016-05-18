948fe1fa6b129f58690ffaff100c7ed612c2bb46
Jd Banares <jandenzelbanares@gmail.com>
2016-05-18 23:49:45 +0800

    Documentation for RoutesFragment

--- app/src/main/java/sp/para/fragments/RoutesFragment.java
+++ app/src/main/java/sp/para/fragments/RoutesFragment.java
@@ -35,6 +35,9 @@ public class RoutesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.routes_fragment, container, false);
 
+        Log.i("RoutesFragment - ", "Setting up routes fragment...");
+
+        // Goes back to previous fragment
         backBtn = (Button) view.findViewById(R.id.backBtn);
 
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +47,7 @@ public class RoutesFragment extends Fragment {
             }
         });
 
+        // Assign an adapter for search bar
         final ArrayAdapter<Route> routeAdapter = new ArrayAdapter<Route>(getActivity(), android.R.layout.simple_list_item_1, Route.getAll());
 
         routeSearch = (AutoCompleteTextView) view.findViewById(R.id.routeSearch);
@@ -53,12 +57,15 @@ public class RoutesFragment extends Fragment {
         routeSearch.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("RoutesFragment - ", "Search item clicked!");
                 View currView = getActivity().getCurrentFocus();
+                // Closes on-screen keyboard if visible
                 if(currView != null) {
                     InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                     inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 }
 
+                // Goes to map fragment
                 MapFragment mf = (MapFragment) getFragmentManager().findFragmentByTag("map_frag");
                 mf.showExisting(routeAdapter.getItem(position));
             }
@@ -66,17 +73,21 @@ public class RoutesFragment extends Fragment {
 
         listRoutes = (ListView) view.findViewById(R.id.listRoutes);
 
+        // Assign an adapter for list view
         listRoutes.setAdapter(routeAdapter);
 
         listRoutes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("RoutesFragment - ", "List item clicked!");
                 View currView = getActivity().getCurrentFocus();
+                // Closes on-screen keyboard if visible
                 if(currView != null) {
                     InputMethodManager inputManager = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
                     inputManager.hideSoftInputFromWindow(getActivity().getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                 }
 
+                // Goes to map fragment
                 MapFragment mf = (MapFragment) getFragmentManager().findFragmentByTag("map_frag");
                 mf.showExisting(routeAdapter.getItem(position));
             }
